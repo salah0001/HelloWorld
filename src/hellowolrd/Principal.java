2f4a45c3387ed5f3831cac0bd11cd4c8a115aa78
Salah <salsma2@alu.edu.gva.es>
2023-10-31 15:02:36 +0100

    Hello

--- src/hellowolrd/Principal.java
+++ src/hellowolrd/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 
  
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");    }
+        System.out.println("HelloWorld!");    
+    }
     
 }
